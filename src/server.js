1c880d5e3537ffac53b56b3b9feaedd1b296bb04
kmositua <kmositua@gmail.com>
2024-11-27 19:13:07 +0000

    creat files

--- src/server.js
+++ src/server.js
@@ -2,7 +2,10 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+//import { env } from './utils/env.js';
 
 dotenv.config();
 
@@ -22,50 +25,11 @@ export const startServer = () => {
     }),
   );
 
-  app.get('/contacts', async (req, res) => {
-    const contacts = await getAllContacts();
+  app.use(contactsRouter); // Додаємо роутер до app як middleware
 
-    res.status(200).json({
-      status: 200,
-      data: contacts,
-      message: 'Successfully found contacts!',
-    });
-  });
+  app.use('*', notFoundHandler);
 
-  app.get('/contacts/:contactId', async (req, res, next) => {
-    const { contactId } = req.params;
-    console.log(contactId);
-    const contact = await getContactById(contactId);
-
-    // Відповідь, якщо контакт не знайдено
-    if (!contact) {
-      res.status(404).json({
-        message: 'Contact not found',
-      });
-      return;
-    }
-
-    // Відповідь, якщо контакт знайдено
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
-
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
-
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
-
+  app.use(errorHandler);
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
