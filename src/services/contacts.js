e75eb2c2a7267794cff837fef2cf8382f107b4a3
kmositua <kmositua@gmail.com>
2024-11-29 13:40:55 +0000

    add files

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,30 @@ export const getContactById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
   return contact;
 };
+
+//POST
+export const createContact = async (payload) => {
+  const contact = await ContactsCollection.create(payload);
+  return contact;
+};
+
+//DELETE
+export const deleteContact = async (contactId) => {
+  const contact = await ContactsCollection.findByIdAndDelete(contactId);
+  return contact;
+};
+
+//PATCH
+export const updateContact = async (contactId, payload, options = {}) => {
+  const contact = await ContactsCollection.findByIdAndUpdate(
+    contactId,
+    payload,
+    { new: true },
+  );
+
+  if (!contact) {
+    return null;
+  }
+
+  return contact;
+};
